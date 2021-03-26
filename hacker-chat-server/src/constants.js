4bb5a969176d9cf66865fbaa0c89cdedd17ccc10
Pablo <pablowoina2205@gmail.com>
2021-03-26 13:58:12 -0300

    deploy v1

---
+++ hacker-chat-server/src/constants.js
@@ -0,0 +1,9 @@
+  
+export const constants = {
+  event: {
+      MESSAGE: 'message',
+      NEW_USER_CONNECTED: 'newUserConnected',
+      DISCONNECT_USER: 'disconnectUser',
+      UPDATE_USERS: 'updateUsers'
+  }
+}
